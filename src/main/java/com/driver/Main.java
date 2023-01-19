16f104e3c85f9dd2dea82bc798c05dd73ff9772e
deepaksorout <39903636+deepaksorout@users.noreply.github.com>
2023-01-19 11:27:13 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product P=new Product();
+        P.product(3,5);
+        P.product(2,5,10);
+        P.product(2.0,5.0);
+    }
+    public static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 }
